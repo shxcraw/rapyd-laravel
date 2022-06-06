0394621023c8fb8d66f609267484175abe165c28
shxcraw <ant.karelin@gmail.com>
2022-06-06 18:40:43 +0300

    Update actions.blade.php

--- views/datagrid/actions.blade.php
+++ views/datagrid/actions.blade.php
@@ -1,11 +1,11 @@
 
 
 @if (in_array("show", $actions))
-    <a class="row-action action-show" title="@lang('rapyd::rapyd.show')" href="{!! $uri !!}?show={!! $id !!}"><span class="glyphicon glyphicon-eye-open"> </span></a>
+    <a class="row-action action-show btn btn-primary btn-xs" title="@lang('rapyd::rapyd.show')" href="{!! $uri !!}?show={!! $id !!}"><span class="glyphicon glyphicon-eye-open"> </span> Смотреть</a>
 @endif
 @if (in_array("modify", $actions))
-    <a class="row-action action-modify" title="@lang('rapyd::rapyd.modify')" href="{!! $uri !!}?modify={!! $id !!}"><span class="glyphicon glyphicon-edit"> </span></a>
+    <a class="row-action action-modify btn btn-primary btn-xs" title="@lang('rapyd::rapyd.modify')" href="{!! $uri !!}?modify={!! $id !!}"><span class="glyphicon glyphicon-edit"> </span> Редактировать</a>
 @endif
 @if (in_array("delete", $actions))
-    <a class="row-action action-delete text-danger" title="@lang('rapyd::rapyd.delete')" href="{!! $uri !!}?delete={!! $id !!}"><span class="glyphicon glyphicon-trash"> </span></a>
+    <a class="row-action action-delete text-danger btn btn-danger btn-xs" title="@lang('rapyd::rapyd.delete')" href="{!! $uri !!}?delete={!! $id !!}"><span class="glyphicon glyphicon-trash"> </span> Удалить</a>
 @endif
